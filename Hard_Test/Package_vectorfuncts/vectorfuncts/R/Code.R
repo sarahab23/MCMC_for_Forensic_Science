8dfe6d23bf1695e4374464bda37c4d00824c99a5
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2020-03-16 22:32:35 +0530

    Update Code.R

--- Hard_Test/Package_vectorfuncts/vectorfuncts/R/Code.R
+++ Hard_Test/Package_vectorfuncts/vectorfuncts/R/Code.R
@@ -1,7 +1,9 @@
+#function to find the largest number in a numeric vector
 largest <- function(x){
   ans1 <- larg(x)
   ans1
 }
+#function to find the smallest number in a numeric vector
 smallest <- function(x){
   ans2 <- small(x)
   ans2
